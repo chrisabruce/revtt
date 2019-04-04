2d39cb945cbde5a54b406575801903a146fd2ff7
Chris Bruce <chris@chrisbruce.com>
2019-04-03 22:04:16 -0700

    adds connection object

--- examples/login.rs
+++ examples/login.rs
@@ -1,6 +1,11 @@
+use revtt::Connection;
+
 use dotenv::dotenv;
 use std::env;
 
 fn main() {
     dotenv().ok();
+
+    let r = Connection::new(&env::var("USER").unwrap(), &env::var("PASS").unwrap());
+    println!("revtt: {:?}", r);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+#[derive(Debug)]
+pub struct Connection {
+    username: String,
+    password: String,
+}
+
+impl Connection {
+    pub fn new(username: &str, password: &str) -> Self {
+        Connection {
+            username: username.to_string(),
+            password: password.to_string(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn new() {
+        let r = Connection::new(&"test", &"pass");
+        assert_eq!(r.username, "test");
+        assert_eq!(r.password, "pass");
     }
 }
