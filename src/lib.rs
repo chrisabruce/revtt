18b3cd8a6140230fdb247a40aacc252074085768
Chris Bruce <chris@chrisbruce.com>
2019-04-12 16:36:07 -0700

    adds user-agent

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use reqwest::Client;
+use reqwest::{header, Client};
 
 pub use reqwest::Error;
 
@@ -13,9 +13,13 @@ pub struct Connection {
 
 impl Connection {
     pub fn new(username: &str, password: &str) -> Self {
+        //Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4
+        let mut headers = header::HeaderMap::new();
+        headers.insert(header::USER_AGENT, header::HeaderValue::from_static("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4"));
         Connection {
             client: reqwest::Client::builder()
                 .cookie_store(true)
+                .default_headers(headers)
                 .build()
                 .unwrap(),
             username: username.to_string(),
@@ -26,8 +30,8 @@ impl Connection {
     /// Returns `Error` if login fails, otherwise `Ok()`
     pub fn login(&mut self) -> Result<(), Error> {
         // Need to hit page to get session cookie
-        let _ = self.client.get(&format!("{}/login.php", BASE_URL)).send()?;
-
+        let mut res = self.client.get(&format!("{}/login.php", BASE_URL)).send()?;
+        println!("{:#?}", res.text());
         let params = [
             ("username", self.username.clone()),
             ("password", self.password.clone()),
